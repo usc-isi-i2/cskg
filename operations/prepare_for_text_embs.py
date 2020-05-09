e9f92313d12a73a6b7966bfe4c1064222815bc33
filievski <filip.dbrsk@gmail.com>
2020-05-08 19:10:23 -0700

    prepare to run text embeddings over CSKG

--- operations/prepare_for_text_embs.py
+++ operations/prepare_for_text_embs.py
@@ -5,6 +5,8 @@ from utils import extract_node_data, normalize_relation
 import pandas as pd
 VERSION="004"
 
+from relation_mapping import COMMONSENSE_MAPPING
+
 def write_data(l, f):
     with open(f, 'w') as csvfile:
         spamwriter = csv.writer(csvfile, delimiter='\t')
@@ -19,7 +21,7 @@ def camel_case_split(s):
     start_idx = [0] + start_idx 
     return [s[x: y] for x, y in zip(start_idx, start_idx[1:])] 
 
-dataset='cskg'
+dataset='cn-original'
 
 edges_file='../output_v%s/%s/edges.tsv' % (VERSION, dataset)
 nodes_file=edges_file.replace('edges', 'nodes')
@@ -40,11 +42,13 @@ with open(edges_file, 'r') as csvfile:
 			my_row[2]=node2label[my_row[2]]
 
 			norm_rel=normalize_relation(my_row[1])
-			my_row[1]=' '.join(camel_case_split(norm_rel)).lower().strip()
-			rows.append(my_row)
+			if norm_rel in COMMONSENSE_MAPPING.keys():
+				my_row[1]=COMMONSENSE_MAPPING[norm_rel]
+				#my_row[1]=' '.join(camel_case_split(norm_rel)).lower().strip()
+				rows.append(my_row)
 
 for n, l in node2label.items():
-	row=[n, 'has label', l]
+	row=[n, 'label', l]
 	rows.append(row)
 
 df=pd.DataFrame(rows, columns=cols)
