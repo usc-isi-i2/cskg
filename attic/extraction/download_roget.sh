f4aeb4eae48db19c550816fd39e16d6f44302943
Filip Ilievski <filip.dbrsk@gmail.com>
2020-10-28 10:30:16 -0700

    cleaning up the repo

--- attic/extraction/download_roget.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p ../input/roget
-cd ../input/roget
-wget https://raw.githubusercontent.com/codogogo/explirefit/master/data/constraints/antonyms.txt
-wget https://raw.githubusercontent.com/codogogo/explirefit/master/data/constraints/synonyms.txt
-cd ../extraction
--- attic/refinement/refine.sh
+++
@@ -1,3 +0,0 @@
-python merge.py
-python normalize_labels.py
-python patch_problematic_labels.py
